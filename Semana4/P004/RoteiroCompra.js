7310452e69c74bc3590e5b7a73ac0c3eeb625362
Leitetc <thiagoleite22@hotmail.com>
2023-12-11 17:29:41 -0300

    commit

--- Semana4/P004/RoteiroCompra.js
+++ Semana4/P004/RoteiroCompra.js
@@ -1,25 +1,55 @@
-function capturarInformacoesPacote(event) {
-    const roteiroViagem = event.target.closest('.roteiros-viagens');
-    const destino = roteiroViagem.querySelector('.roteiro-destino h3').innerText;
-    const inclusos = Array.from(roteiroViagem.querySelectorAll('.roteiro-incluso li')).map(li => li.innerText);
-    const preco = roteiroViagem.querySelector('.roteiro-preco').innerText;
-    const obs = roteiroViagem.querySelector('.roteiro-obs').innerText;
-    const parcelamento = roteiroViagem.querySelector('.roteiro-parcelamento').innerText;
+document.addEventListener('DOMContentLoaded', function() {
+    const buttons = document.querySelectorAll('.roteiro-comprar');
 
-    const pacoteTuristico = {
-        destino,
-        inclusos,
-        preco,
-        obs,
-        parcelamento
-    };
+    buttons.forEach(button => {
+        button.addEventListener('click', function() {
+            const roteiro = this.parentElement;
+            const titulo = roteiro.querySelector('.roteiro-destino h3').textContent;
+            const pacote = roteiro.querySelectorAll('.roteiro-incluso li');
+            const valor = roteiro.querySelector('.roteiro-preco').textContent;
+            const comentarios = roteiro.querySelector('.roteiro-obs').textContent;
+            const formaPagamento = roteiro.querySelector('.roteiro-parcelamento').textContent;
+            const imagem = roteiro.querySelector('.postal').getAttribute('src');
 
-    console.log(JSON.stringify(pacoteTuristico));
-}
+            const pacoteTuristico = {
+                titulo: titulo,
+                pacote: Array.from(pacote).map(item => item.textContent),
+                valor: valor,
+                comentarios: comentarios,
+                formaPagamento: formaPagamento,
+                imagem: imagem
+            };
 
-document.addEventListener('DOMContentLoaded', function () {
-    const botoesComprar = document.querySelectorAll('.roteiro-comprar');
-    botoesComprar.forEach(botao => {
-        botao.addEventListener('click', capturarInformacoesPacote);
+            console.log(pacoteTuristico);
+        });
     });
+
+    function inserirRoteiro() {
+        const formulario = document.getElementById('formulario');
+        const containerDestinos = document.getElementById('container-destinos');
+
+        const novoRoteiro = document.createElement('div');
+        novoRoteiro.classList.add('roteiros-viagens');
+
+        const titulo = formulario.querySelector('#titulo').value;
+        const pacote = formulario.querySelector('#Pacote').value.split('\n');
+        const valor = formulario.querySelector('#Valor').value;
+        const comentarios = formulario.querySelector('#Comentarios').value;
+        const formaPagamento = formulario.querySelector('#FormaPagamento').value;
+        const imagem = formulario.querySelector('#imagem').value;
+
+        novoRoteiro.innerHTML = `
+            <img src="${imagem}" class="postal">
+            <div class="roteiro-destino"><h3>${titulo}</h3></div>
+            <ul class="roteiro-incluso">
+                ${pacote.map(item => `<li>${item}</li>`).join('')}
+            </ul>
+            <div class="roteiro-preco">${valor}</div>
+            <div class="roteiro-obs">${comentarios}</div>
+            <div class="roteiro-parcelamento">${formaPagamento}</div>
+            <button class="roteiro-comprar">COMPRAR</button>
+        `;
+
+        containerDestinos.appendChild(novoRoteiro);
+    }
 });
